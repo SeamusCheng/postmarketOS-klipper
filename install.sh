fd3cfc7359aefa64fe4d173317e7d025254445ed
FT256 <80577890+FT256@users.noreply.github.com>
2022-05-06 23:33:32 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 
 set -euxo pipefail
 
-: ${CONFIG_PATH:="$HOME/config"}
+: ${CONFIG_PATH:="$HOME/klipper_config"}
 : ${GCODE_PATH:="$HOME/gcode"}
 
 : ${KLIPPER_REPO:="https://github.com/KevinOConnor/klipper.git"}
 : ${KLIPPER_PATH:="$HOME/klipper"}
-: ${KLIPPY_VENV_PATH:="$HOME/venv/klippy"}
+: ${KLIPPY_VENV_PATH:="$HOME/klippy-env"}
 
 : ${MOONRAKER_REPO:="https://github.com/Arksine/moonraker"}
 : ${MOONRAKER_PATH:="$HOME/moonraker"}
-: ${MOONRAKER_VENV_PATH:="$HOME/venv/moonraker"}
+: ${MOONRAKER_VENV_PATH:="$HOME/moonraker-env"}
 
-: ${CLIENT:="fluidd"}
+: ${CLIENT:=""}
 : ${CLIENT_PATH:="$HOME/www"}
 
 if [ $(id -u) = 0 ]; then
@@ -36,10 +36,10 @@ sudo setup-udev
 
 case $CLIENT in
   fluidd)
-    CLIENT_RELEASE_URL=`curl -s https://api.github.com/repos/cadriel/fluidd/releases | jq -r ".[0].assets[0].browser_download_url"`
+    CLIENT_RELEASE_URL=`curl -s https://api.github.com/repos/fluidd-core/fluidd/releases | jq -r ".[0].assets[0].browser_download_url"`
     ;;
   mainsail)
-    CLIENT_RELEASE_URL=`curl -s https://api.github.com/repos/meteyou/mainsail/releases | jq -r ".[0].assets[0].browser_download_url"`
+    CLIENT_RELEASE_URL=`curl -s https://api.github.com/repos/mainsail-crew/mainsail/releases | jq -r ".[0].assets[0].browser_download_url"`
     ;;
   *)
     echo "Unknown client $CLIENT (choose fluidd or mainsail)"
@@ -79,13 +79,22 @@ sudo apk add libsodium curl-dev
 
 test -d $MOONRAKER_PATH || git clone $MOONRAKER_REPO $MOONRAKER_PATH
 test -d $MOONRAKER_VENV_PATH || virtualenv -p python3 $MOONRAKER_VENV_PATH
+
+sed '/lmdb/d' $MOONRAKER_PATH/scripts/moonraker-requirements.txt
+git clone https://github.com/jnwatson/py-lmdb /tmp/lmdb
+source $MOONRAKER_VENV_PATH/bin/activate
+LMDB_FORCE_SYSTEM=1 python3 /tmp/lmdb/setup.py build
+pytest -k 'not testIterWithDeletes' $(echo /tmp/lmdb/build/lib*)
+LMDB_FORCE_SYSTEM=1 python3 /tmp/lmdb/setup.py install
+deactivate
+
 $MOONRAKER_VENV_PATH/bin/python -m pip install --upgrade pip
 $MOONRAKER_VENV_PATH/bin/pip install -r $MOONRAKER_PATH/scripts/moonraker-requirements.txt
 
 sudo tee /etc/init.d/moonraker <<EOF
 #!/sbin/openrc-run
 command="$MOONRAKER_VENV_PATH/bin/python"
-command_args="$MOONRAKER_PATH/moonraker/moonraker.py"
+command_args="$MOONRAKER_PATH/moonraker/moonraker.py -c $CONFIG_PATH/moonraker.conf -l /tmp/moonraker.log"
 command_background=true
 command_user="$USER"
 pidfile="/run/moonraker.pid"
@@ -99,20 +108,39 @@ sudo chmod a+x /etc/init.d/moonraker
 cat > $HOME/moonraker.conf <<EOF
 [server]
 host: 0.0.0.0
+port: 7125
+# Verbose logging used for debugging . Default False.
+enable_debug_logging: True
+# The maximum size allowed for a file upload (in MiB).  Default 1024 MiB
+max_upload_size: 1024
+
+[machine]
+provider: none
+
+[file_manager]
 config_path: $CONFIG_PATH
+log_path: /tmp/klipper_logs
+# post processing for object cancel. Not recommended for low resource SBCs such as a Pi Zero. Default False
+enable_object_processing: False
+
+[database]
+database_path: $HOME/.moonraker_database
 
 [authorization]
+cors_domains:
+    https://my.mainsail.xyz
+    http://my.mainsail.xyz
+    http://*.local
+    http://*.lan
 trusted_clients:
-  192.168.1.0/24
-
-[octoprint_compat]
-
-[update_manager]
-
-[update_manager client fluidd]
-type: web
-repo: cadriel/fluidd
-path: ~/www
+    10.0.0.0/8
+    127.0.0.0/8
+    169.254.0.0/16
+    172.16.42.0/16
+    192.168.0.0/16
+    192.168.0.254
+    FE80::/10
+    ::1/128
 EOF
 
 sudo rc-update add moonraker
@@ -155,20 +183,6 @@ mkdir -p $CLIENT_PATH
 
 sudo rc-update add caddy
 sudo service caddy start
-
-################################################################################
-# AUTO DELETE OLD GCODE
-################################################################################
-
-sudo tee /etc/periodic/15min/klipper <<END
-#!/bin/sh
-find $GCODE_PATH -mtime +5 -type f -delete
-END
-
-sudo chmod a+x /etc/periodic/15min/klipper
-
-sudo service crond start
-sudo rc-update add crond
 
 # UPDATE SCRIPT
 
